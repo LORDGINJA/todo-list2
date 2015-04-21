b5d712e46c4651a118c6fe247e4d2082391b4fa7
Maxwell Karp <maxwell.karp@yahoo.com>
2015-04-21 11:46:40 -0700

    video 8 and 9

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Todo-Lists</title>
+	<title>Todo-List2</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>	
@@ -20,4 +20,23 @@
 		<input class="words" type="text" name="new-task" placeholder="Add new item..."/> 
 	</form>
 </body>
+<script>
+		//calls add_task function 
+		add_task();
+		//creates add_task function
+		function add_task(){
+			$('.add-new-task').submit(function(){
+				var new_task = $('.add-new-task input[name=new-task]').val();
+
+				if (new_task != '') {
+					//goes to add-task.php
+					$.post('includes/add-tasks.php', {task: new_task}, function(data){
+						$('add-new-task input[name=new-task]').val();
+						$(data).appendTo('.task-list ul').hide().fadeIn();
+					});
+				}
+				return false;
+			});
+		}
+	</script>
 </html>
