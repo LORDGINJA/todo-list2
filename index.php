be46bc91d12a020bd963c1ac94b2cd0889f24823
Maxwell Karp <maxwell.karp@yahoo.com>
2015-04-21 11:20:17 -0700

    video 6

--- index.php
+++ index.php
@@ -6,8 +6,18 @@
 </head>
 <body>	
 	<div class="wrap">
-		
+		<div class="task-list">
+			<ul>
+				<?php 
+					// inserts connect.php file into this div
+					require("includes/connect.php"); 
+				?>
+			</ul>
+		</div>
 	</div>
-
+	<!-- inserts a box for inserting text -->
+	<form class="add-new-task words" autocomplete = "off">
+		<input class="words" type="text" name="new-task" placeholder="Add new item..."/> 
+	</form>
 </body>
 </html>
